752ee54ca35f49ca90672236c65d6fdce608fac3
Mateusz Dudzinski <mateuszpd7@gmail.com>
2018-01-17 00:34:45 +0100

    Fixed bug with gcd and improved indentation.

--- przelewanka/przelewanka.ml
+++ przelewanka/przelewanka.ml
@@ -14,10 +14,10 @@ let przelewanka input_arr =
      do hash-mapy. Dodawana jest KOPIA tablicy arr (nowa tablica). *)
   let try_to_add arr dist = 
     if not (Hashtbl.mem hash_tab arr) then begin
-      let copy = Array.copy arr in
-      Hashtbl.add hash_tab copy (dist + 1);
-      Queue.push copy queue
-    end
+        let copy = Array.copy arr in
+        Hashtbl.add hash_tab copy (dist + 1);
+        Queue.push copy queue
+      end
   in
   (* Funckje zlewania, dolewania i przelewania opisuja wyszstkie mozliwe
      ruchy z przekazanego im stanu. Funkcje te modyfikuja tablice, 
@@ -34,13 +34,13 @@ let przelewanka input_arr =
     try_to_add arr dist;
     arr.(k) <- tmp
   and transfer arr k j dist =
-      let tmp_k, tmp_j = arr.(k), arr.(j) 
-      and free_space = capacity.(j) - arr.(j) in
-      arr.(j) <- arr.(j) + min free_space arr.(k);
-      arr.(k) <- max (arr.(k) - free_space) 0;
-      try_to_add arr dist;
-      arr.(j) <- tmp_j;
-      arr.(k) <- tmp_k
+    let tmp_k, tmp_j = arr.(k), arr.(j) 
+    and free_space = capacity.(j) - arr.(j) in
+    arr.(j) <- arr.(j) + min free_space arr.(k);
+    arr.(k) <- max (arr.(k) - free_space) 0;
+    try_to_add arr dist;
+    arr.(j) <- tmp_j;
+    arr.(k) <- tmp_k
   in
   (* Przetworzenie pojedynczego stanu osiagalnego w dist ruchow. *)
   let process_state curr_state dist = 
@@ -56,7 +56,7 @@ let przelewanka input_arr =
         (* Jesli i-ta szklanka niepusta i j-ta niepelna 
            to przelewamy z i do j. *)
         if i <> j 
-        && next_state.(i) > 0 && next_state.(j) < capacity.(j) then
+           && next_state.(i) > 0 && next_state.(j) < capacity.(j) then
           transfer next_state i j dist
       done
     done
@@ -64,24 +64,24 @@ let przelewanka input_arr =
   in
   (* Jesli nie ma szklanek lub wszyskie maja pojemosc 0 nie ma co robic. *)
   if len = 0 || common_div = 0 then 0
-  (* kazda wielkosc tablicy docelowej dzieli sie przez nwd pojemnosci szklanek 
+  (* kazda wielkosc tablicy docelowej dzieli sie przez nwd pojemnosci szklanek
      oraz music byc jedna szkanka ktora jest pusta/pelna bo musimy skonczyc *)
-  else if not (Array.for_all (fun (x, _) -> x mod common_div = 0) input_arr)
-  || not (Array.exists (fun (x, y) -> y = 0 || x = y) input_arr) then
+  else if not (Array.for_all (fun (_, y) -> y mod common_div = 0) input_arr)
+          || not (Array.exists (fun (x, y) -> y = 0 || x = y) input_arr) then
     -1
   else begin
-    (* Stany poczatkowy i koncowy: *)
-    let inicial_state = Array.make len 0 
-    and goal_state = Array.map snd input_arr in
-    (* Inicializacja struktur danych *)
-    Queue.push inicial_state queue;
-    Hashtbl.add hash_tab inicial_state 0; 
-    (* Wykonujemy BFS. Z wlasnosci tego przeszukiwania wiemy,ze gdy znajdziemy
-       poszukiwany wynik bedzie osiagalny optymalnym kosztem. *)
-    while not (Queue.is_empty queue) && Queue.top queue <> goal_state do
-      let curr_state = Queue.pop queue in
-      process_state curr_state (Hashtbl.find hash_tab curr_state)
-    done;
-    try Hashtbl.find hash_tab goal_state with
-    | Not_found -> -1
-  end;;
+      (* Stany poczatkowy i koncowy: *)
+      let inicial_state = Array.make len 0 
+      and goal_state = Array.map snd input_arr in
+      (* Inicializacja struktur danych *)
+      Queue.push inicial_state queue;
+      Hashtbl.add hash_tab inicial_state 0; 
+      (* Wykonujemy BFS. Z wlasnosci tego przeszukiwania wiemy,ze gdy 
+         znajdziemy poszukiwany wynik bedzie osiagalny optymalnym kosztem. *)
+      while not (Queue.is_empty queue) && Queue.top queue <> goal_state do
+        let curr_state = Queue.pop queue in
+        process_state curr_state (Hashtbl.find hash_tab curr_state)
+      done;
+      try Hashtbl.find hash_tab goal_state with
+      | Not_found -> -1
+    end;;
